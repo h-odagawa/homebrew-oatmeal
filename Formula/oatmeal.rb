e51cfdf865f36bd1c7dc8ea0eec02b9e196b3a1f
Haruki Odagawa <odagawa.haruki.og0@naist.ac.jp>
2026-08-05 13:05:08 +0900

    oatmeal公開用の設定を追加

---
+++ Formula/oatmeal.rb
@@ -0,0 +1,22 @@
+class oatmeal < Formula
+  desc "A simple tool for easy serial connections."
+
+  # スクリプトのリポジトリ
+  homepage "https://github.com/H-odagawa/oatmeal"
+
+  # GitHub がタグごとに生成するgzファイルを指定
+  url "https://github.com/h-odagawa/oatmeal/releases/download/v1.0.0/oatmeal-aarch64-apple-darwin.tar.gz" 
+
+  # 先ほど得られたハッシュ
+  sha256 "7507c0674a9fcdbed9891b1a0de6d38f2b8d36f3b80e9b627f94c6799b23b270"
+
+  license "MIT"
+
+  def install
+    bin.install "bin/oatmeal"
+  end
+
+  test do
+    system "true"
+  end
+end
